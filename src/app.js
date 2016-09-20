0a3b6dd495affb8174b49a0641ec49bcb00a1ac9
Marshall Thompson <marshall@creativeideal.net>
2016-09-20 11:10:39 -0600

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { connect } from '@bigab/can-react';
+import DefineMap from 'can-define/map/';
+import AppHome from './app.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const ViewModel = DefineMap.extend({
+  message: {
+    type: 'string',
+    value: 'Hello, Feathers!'
+  }
+});
+
+const AppContainer = connect(ViewModel, AppHome);
+
+// Reset the DOM for live-reloading.
+document.querySelector('[root=true]').innerHTML = '';
+
+// Render the DOM
+ReactDOM.render(
+  <AppContainer />,
+  document.querySelector('[root=true]')
+);
