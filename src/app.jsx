30eb8e76bf5c075198534da81771d5907c3a7a29
Marshall Thompson <marshall@creativeideal.net>
2017-01-03 12:05:01 -0700

    Add about page

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Auth from '~/components/auth/';
 import Dashboard from '~/components/dashboard/';
+import Menu from '~/components/menu/';
 import Home from '~/components/home/';
+import About from '~/components/about/';
 import {Route} from 'can-route-react';
 
-export default ({page, changePage, setRoute}) => {
+export default ({page, changePage, session}) => {
   return (
     <div>
-      <button type='button' onClick={changePage('home')}> Home </button>
-      <button type='button' onClick={changePage('dashboard')}> Dashboard </button>
-      <button type='button' onClick={changePage('signup')}> Signup </button>
-      <button type='button' onClick={changePage('login')}> Login </button>
+      <Menu page={page} changePage={changePage} />
 
-      <Route data={{page: 'home'}} Component={Home} />
+      <Route data={{page: 'home'}} Component={<Home changePage={changePage} />} />
+      <Route data={{page: 'about'}} Component={About} />
       <Route data={{page: 'dashboard'}} Component={Dashboard} />
       { (page === 'login' || page === 'signup') && <Auth tab={page} /> }
     </div>
---
+++ src/components/about/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <section className='about-page'>
+      <div style={{paddingTop: '10px'}}>Not much to learn about us.</div>
+    </section>
+  );
+};
