fc5dd1a5985ec892aedfc399e39b64a137de4252
Marshall Thompson <marshall@creativeideal.net>
2017-01-02 17:19:18 -0700

    Update feathers client module.

---
+++ src/models/feathers-client.js
@@ -0,0 +1,30 @@
+import feathers from 'feathers/client';
+import socketio from 'feathers-socketio/client';
+import hooks from 'feathers-hooks';
+import io from 'steal-socket.io';
+import rxjs from 'rxjs';
+import rx from 'feathers-reactive';
+import auth from 'feathers-authentication-client';
+
+const host = 'http://localhost:3030';
+const socket = io(host, {
+  transports: ['websocket'],
+  forceReconnect: true
+});
+const app = feathers()
+  .configure(rx(rxjs, {
+    idField: '_id'
+  }))
+  .configure(hooks())
+  .configure(socketio(socket))
+  .configure(auth({
+    header: 'authorization', // the default authorization header
+    path: '/authentication', // the server side authentication service path
+    jwtStrategy: 'jwt', // the name of the JWT authentication strategy 
+    entity: 'user', // the entity you are authenticating (ie. a users)
+    service: 'users', // the service to look up the entity
+    cookie: 'feathers-jwt', // the name of the cookie to parse the JWT from when cookies are enabled server side
+    storageKey: 'feathers-jwt' // the key to store the accessToken in localstorage or AsyncStorage on React Native
+  }));
+
+export default app;
