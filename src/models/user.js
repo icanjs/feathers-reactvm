dcaf90cb4c10b284b09873747970485b3e429f62
Marshall Thompson <marshall@creativeideal.net>
2017-01-02 17:18:58 -0700

    Add User Model.

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+import DefineMap from 'can-define/map/';
+import DefineList from 'can-define/list/list';
+import feathersClient from './feathers-client';
+import superModel from '../lib/super-model';
+import algebra from './algebras';
+
+var User = DefineMap.extend('User', {
+  _id: 'string',
+  email: {
+    type: 'string'
+  },
+  password: {
+    type: 'string'
+  }
+});
+
+User.List = DefineList.extend({
+  '#': User
+});
+
+User.connection = superModel({
+  Map: User,
+  List: User.List,
+  feathersService: feathersClient.service('/users'),
+  name: 'users',
+  algebra
+});
+
+User.algebra = algebra;
+
+export default User;
