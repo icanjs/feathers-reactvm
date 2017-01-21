ae0b08f14d602c914313b6e452a73f2ee2ac8b39
Marshall Thompson <marshall@creativeideal.net>
2017-01-20 20:28:06 -0700

    Remove Route component.

--- src/app.jsx
+++ src/app.jsx
@@ -4,16 +4,15 @@ import Dashboard from '~/components/dashboard/';
 import Menu from '~/components/menu/';
 import Home from '~/components/home/';
 import About from '~/components/about/';
-import {Route} from 'can-route-react';
 
 export default ({page, changePage, session}) => {
   return (
     <div>
       <Menu page={page} changePage={changePage} />
 
-      <Route data={{page: 'home'}} Component={<Home changePage={changePage} />} />
-      <Route data={{page: 'about'}} Component={About} />
-      <Route data={{page: 'dashboard'}} Component={Dashboard} />
+      { page === 'home' && <Home changePage={changePage} />}
+      { page === 'about' && <About />}
+      { page === 'dashboard' && <Dashboard /> }
       { (page === 'login' || page === 'signup') && <Auth tab={page} /> }
     </div>
   );
