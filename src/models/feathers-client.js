8496651c08078c5c96f5f72ac4d49a95c2d188bd
Marshall Thompson <marshall@creativeideal.net>
2017-01-25 13:35:38 -0700

    Store JWT in localStorage.

--- src/models/feathers-client.js
+++ src/models/feathers-client.js
@@ -24,6 +24,7 @@ const app = feathers()
     entity: 'user', // the entity you are authenticating (ie. a users)
     service: 'users', // the service to look up the entity
     cookie: 'feathers-jwt', // the name of the cookie to parse the JWT from when cookies are enabled server side
+    storage: window.localStorage,
     storageKey: 'feathers-jwt' // the key to store the accessToken in localstorage or AsyncStorage on React Native
   }));
 
